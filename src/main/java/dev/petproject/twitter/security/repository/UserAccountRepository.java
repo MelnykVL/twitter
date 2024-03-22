e265356035ca9f16ce378b894fccd96666ca06be
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-23 00:41:41 +0200

    Added existsByUsername method to UserAccountRepository.java

--- src/main/java/dev/petproject/twitter/security/repository/UserAccountRepository.java
+++ src/main/java/dev/petproject/twitter/security/repository/UserAccountRepository.java
@@ -3,4 +3,7 @@ package dev.petproject.twitter.security.repository;
 import dev.petproject.twitter.security.model.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {}
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+
+    boolean existsByUsername(String username);
+}
