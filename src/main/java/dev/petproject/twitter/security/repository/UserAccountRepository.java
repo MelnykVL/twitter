290ebd18db09ddb2d0969c7f2b76ae19157ce13c
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-22 21:30:20 +0200

    Created UserAccountRepository.java

---
+++ src/main/java/dev/petproject/twitter/security/repository/UserAccountRepository.java
@@ -0,0 +1,6 @@
+package dev.petproject.twitter.security.repository;
+
+import dev.petproject.twitter.security.model.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {}
