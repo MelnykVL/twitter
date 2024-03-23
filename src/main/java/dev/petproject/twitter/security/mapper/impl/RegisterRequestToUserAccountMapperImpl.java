a65970dfecb06d8a31fa88057678a6dcd524fc3f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-24 00:58:45 +0200

    Created RegisterRequestToUserAccountMapperImpl.java

---
+++ src/main/java/dev/petproject/twitter/security/mapper/impl/RegisterRequestToUserAccountMapperImpl.java
@@ -0,0 +1,38 @@
+package dev.petproject.twitter.security.mapper.impl;
+
+import dev.petproject.twitter.security.mapper.RegisterRequestToUserAccountMapper;
+import dev.petproject.twitter.security.model.UserAccount;
+import dev.petproject.twitter.security.model.UserRole;
+import dev.petproject.twitter.security.service.UserRoleService;
+import dev.petproject.twitter.security.web.model.RegisterRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+import java.util.Set;
+
+@Component
+public class RegisterRequestToUserAccountMapperImpl implements RegisterRequestToUserAccountMapper {
+
+    private final UserRoleService userRoleService;
+    private final PasswordEncoder passwordEncoder;
+
+    public RegisterRequestToUserAccountMapperImpl(UserRoleService userRoleService, PasswordEncoder passwordEncoder) {
+        this.userRoleService = userRoleService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserAccount map(RegisterRequest registerRequest) {
+        UserRole userRole = this.userRoleService.findUserRole()
+                .orElseThrow(() -> new RuntimeException("User role not found"));
+
+        UserAccount userAccount = new UserAccount();
+        userAccount.setUsername(registerRequest.username()
+                .toLowerCase(Locale.ROOT));
+        userAccount.setPassword(this.passwordEncoder.encode(registerRequest.password()));
+        userAccount.setAuthorities(Set.of(userRole));
+
+        return userAccount;
+    }
+}
