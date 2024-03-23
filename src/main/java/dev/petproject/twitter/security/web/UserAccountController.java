edaeb94299687b2b263e50fceb716e85f860d2f8
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-24 01:20:32 +0200

    Refactored UserAccountController.java

--- src/main/java/dev/petproject/twitter/security/web/UserAccountController.java
+++ src/main/java/dev/petproject/twitter/security/web/UserAccountController.java
@@ -1,44 +1,24 @@
 package dev.petproject.twitter.security.web;
 
-import dev.petproject.twitter.security.model.UserAccount;
-import dev.petproject.twitter.security.model.UserRole;
-import dev.petproject.twitter.security.service.UserAccountService;
-import dev.petproject.twitter.security.service.UserRoleService;
+import dev.petproject.twitter.security.usecase.impl.RegisterUserAccountUseCaseFacade;
 import dev.petproject.twitter.security.web.model.RegisterRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Locale;
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/v1/accounts")
 public class UserAccountController {
 
-    private final UserAccountService userAccountService;
-    private final UserRoleService userRoleService;
-    private final PasswordEncoder passwordEncoder;
+    private final RegisterUserAccountUseCaseFacade registerRequestToUserAccountMapper;
 
-    public UserAccountController(UserAccountService userAccountService, UserRoleService userRoleService, PasswordEncoder passwordEncoder) {
-        this.userAccountService = userAccountService;
-        this.userRoleService = userRoleService;
-        this.passwordEncoder = passwordEncoder;
+    public UserAccountController(RegisterUserAccountUseCaseFacade registerRequestToUserAccountMapper) {
+        this.registerRequestToUserAccountMapper = registerRequestToUserAccountMapper;
     }
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public void registerAccount(@Valid @RequestBody RegisterRequest registerRequest) {
-        UserRole userRole = this.userRoleService.findUserRole()
-                .orElseThrow(() -> new RuntimeException("User role not found"));
-
-        UserAccount userAccount = new UserAccount();
-        userAccount.setUsername(registerRequest.username()
-                .toLowerCase(Locale.ROOT));
-        userAccount.setPassword(this.passwordEncoder.encode(registerRequest.password()));
-        userAccount.setAuthorities(Set.of(userRole));
-
-        this.userAccountService.createUserAccount(userAccount);
+        registerRequestToUserAccountMapper.register(registerRequest);
     }
 }
