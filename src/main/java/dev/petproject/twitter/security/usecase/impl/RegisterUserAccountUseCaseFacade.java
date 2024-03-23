0aa895ac5f59d0e12fa722d1017f913fef931745
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-24 01:05:03 +0200

    Created RegisterUserAccountUseCaseFacade.java

---
+++ src/main/java/dev/petproject/twitter/security/usecase/impl/RegisterUserAccountUseCaseFacade.java
@@ -0,0 +1,25 @@
+package dev.petproject.twitter.security.usecase.impl;
+
+import dev.petproject.twitter.security.mapper.RegisterRequestToUserAccountMapper;
+import dev.petproject.twitter.security.model.UserAccount;
+import dev.petproject.twitter.security.service.UserAccountService;
+import dev.petproject.twitter.security.usecase.RegisterUserAccountUseCase;
+import dev.petproject.twitter.security.web.model.RegisterRequest;
+
+public class RegisterUserAccountUseCaseFacade implements RegisterUserAccountUseCase {
+
+    private final UserAccountService userAccountService;
+    private final RegisterRequestToUserAccountMapper registerRequestToUserAccountMapper;
+
+    public RegisterUserAccountUseCaseFacade(UserAccountService userAccountService,
+            RegisterRequestToUserAccountMapper registerRequestToUserAccountMapper) {
+        this.userAccountService = userAccountService;
+        this.registerRequestToUserAccountMapper = registerRequestToUserAccountMapper;
+    }
+
+    @Override
+    public void register(RegisterRequest registerRequest) {
+        UserAccount userAccount = this.registerRequestToUserAccountMapper.map(registerRequest);
+        this.userAccountService.createUserAccount(userAccount);
+    }
+}
