b70b0dda63b6bd3b1f0a6854ceb2b0c97e3b84fd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-24 00:59:03 +0200

    Created RegisterUserAccountUseCase interface

---
+++ src/main/java/dev/petproject/twitter/security/usecase/RegisterUserAccountUseCase.java
@@ -0,0 +1,8 @@
+package dev.petproject.twitter.security.usecase;
+
+import dev.petproject.twitter.security.web.model.RegisterRequest;
+
+public interface RegisterUserAccountUseCase {
+
+    void register(RegisterRequest registerRequest);
+}
