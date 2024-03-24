d796ac1559dbda1c89bbc2d187928d4f03c56f5c
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 01:28:29 +0200

    Created UserDetailsServiceImpl.java

---
+++ src/main/java/dev/petproject/twitter/security/service/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package dev.petproject.twitter.security.service.impl;
+
+import dev.petproject.twitter.security.mapper.UserAccountToUserMapper;
+import dev.petproject.twitter.security.service.UserAccountService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserAccountService userAccountService;
+    private final UserAccountToUserMapper mapper;
+
+    public UserDetailsServiceImpl(UserAccountService userAccountService, UserAccountToUserMapper mapper) {
+        this.userAccountService = userAccountService;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userAccountService.findUserByUsername(username)
+                .map(this.mapper::map)
+                .orElseThrow(() -> new UsernameNotFoundException("Bad credentials"));
+    }
+}
