30955fb0dc4fba13340262a8cad07aeee107cb19
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 19:56:28 +0200

    Created UserProfileService.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/service/UserProfileService.java
@@ -0,0 +1,8 @@
+package dev.petproject.twitter.user.profile.service;
+
+import dev.petproject.twitter.user.profile.model.UserProfile;
+
+public interface UserProfileService {
+
+    void createUserProfile(UserProfile userProfile);
+}
