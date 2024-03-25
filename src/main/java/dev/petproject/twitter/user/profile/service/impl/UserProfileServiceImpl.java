7f72dac9977c732bfb77528beea0b88d5891c0cc
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 19:56:40 +0200

    Created UserProfileServiceImpl.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/service/impl/UserProfileServiceImpl.java
@@ -0,0 +1,31 @@
+package dev.petproject.twitter.user.profile.service.impl;
+
+import dev.petproject.twitter.user.profile.model.UserProfile;
+import dev.petproject.twitter.user.profile.repository.UserProfileRepository;
+import dev.petproject.twitter.user.profile.service.UserProfileService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileServiceImpl implements UserProfileService {
+
+    private final UserProfileRepository userProfileRepository;
+
+    public UserProfileServiceImpl(UserProfileRepository userProfileRepository) {
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    @Override
+    public void createUserProfile(UserProfile userProfile) {
+        if (this.userProfileRepository.existsById(userProfile.getId())) {
+            String errorMessage = String.format("User profile with specified Id = %d already exists",
+                                                userProfile.getId());
+            throw new RuntimeException(errorMessage);
+        }
+        if (this.userProfileRepository.existsByNickname(userProfile.getNickname())) {
+            String errorMessage = String.format("User profile with specified Nickname = %s already exists",
+                                                userProfile.getNickname());
+            throw new RuntimeException(errorMessage);
+        }
+        this.userProfileRepository.save(userProfile);
+    }
+}
