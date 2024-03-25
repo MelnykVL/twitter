b752421e65a6ae04f09296d2399a021c849051b5
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-26 00:04:54 +0200

    Created IdentityApiService.java

---
+++ src/main/java/dev/petproject/twitter/security/api/service/IdentityApiService.java
@@ -0,0 +1,10 @@
+package dev.petproject.twitter.security.api.service;
+
+import dev.petproject.twitter.security.api.model.CurrentUserApiModel;
+
+import java.util.Optional;
+
+public interface IdentityApiService {
+
+    Optional<CurrentUserApiModel> currentUserAccount();
+}
