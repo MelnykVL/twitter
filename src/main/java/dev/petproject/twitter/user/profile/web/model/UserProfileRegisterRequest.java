d8988ce1dde4ab75b937b197ce002139769776ba
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 19:57:26 +0200

    Created UserProfileRegisterRequest.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/web/model/UserProfileRegisterRequest.java
@@ -0,0 +1,5 @@
+package dev.petproject.twitter.user.profile.web.model;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record UserProfileRegisterRequest(@NotBlank String nickname, @NotBlank String imageLink) {}
