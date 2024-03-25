2fc811853e7c610bd33c8f8a016c4b40cd23205f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 19:56:14 +0200

    Created UserProfileRepository.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/repository/UserProfileRepository.java
@@ -0,0 +1,9 @@
+package dev.petproject.twitter.user.profile.repository;
+
+import dev.petproject.twitter.user.profile.model.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
+
+    boolean existsByNickname(String nickname);
+}
