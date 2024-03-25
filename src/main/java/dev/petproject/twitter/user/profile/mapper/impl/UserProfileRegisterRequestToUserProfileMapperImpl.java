6dc331d22e8634d558bdf07b08e428b534f21f20
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-26 00:06:14 +0200

    Removed mocked id of UserProfile and set it by IdentityApiService

--- src/main/java/dev/petproject/twitter/user/profile/mapper/impl/UserProfileRegisterRequestToUserProfileMapperImpl.java
+++ src/main/java/dev/petproject/twitter/user/profile/mapper/impl/UserProfileRegisterRequestToUserProfileMapperImpl.java
@@ -1,5 +1,7 @@
 package dev.petproject.twitter.user.profile.mapper.impl;
 
+import dev.petproject.twitter.security.api.model.CurrentUserApiModel;
+import dev.petproject.twitter.security.api.service.IdentityApiService;
 import dev.petproject.twitter.user.profile.mapper.UserProfileRegisterRequestToUserProfileMapper;
 import dev.petproject.twitter.user.profile.model.UserProfile;
 import dev.petproject.twitter.user.profile.web.model.UserProfileRegisterRequest;
@@ -8,10 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserProfileRegisterRequestToUserProfileMapperImpl implements UserProfileRegisterRequestToUserProfileMapper {
 
+    private final IdentityApiService identityApiService;
+
+    public UserProfileRegisterRequestToUserProfileMapperImpl(IdentityApiService identityApiService) {
+        this.identityApiService = identityApiService;
+    }
+
     @Override
     public UserProfile map(UserProfileRegisterRequest registerRequest) {
+        CurrentUserApiModel currentUserApiModel = this.identityApiService.currentUserAccount()
+                .orElseThrow(() -> new RuntimeException("To create a user profile, user have to log in to the system"));
+
         UserProfile userProfile = new UserProfile();
-        userProfile.setId(1L);
+        userProfile.setId(currentUserApiModel.userAccountId());
         userProfile.setNickname(registerRequest.nickname());
         userProfile.setImageLink(registerRequest.imageLink());
 
