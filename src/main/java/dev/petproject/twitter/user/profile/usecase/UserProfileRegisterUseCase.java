96556c07a9574152f096ed75caaa7ff0834bbf56
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-25 19:56:59 +0200

    Created UserProfileRegisterUseCase.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/usecase/UserProfileRegisterUseCase.java
@@ -0,0 +1,8 @@
+package dev.petproject.twitter.user.profile.usecase;
+
+import dev.petproject.twitter.user.profile.web.model.UserProfileRegisterRequest;
+
+public interface UserProfileRegisterUseCase {
+
+    void registerUserProfile(UserProfileRegisterRequest userProfileRegisterRequest);
+}
