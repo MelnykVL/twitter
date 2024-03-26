5f0888c1ca998128bf1cf5614ee92d05d6705691
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 01:02:15 +0200

    Finished TweetAddRequestToTweetMapperImpl.java

--- src/main/java/dev/petproject/twitter/user/tweet/mapper/impl/TweetAddRequestToTweetMapperImpl.java
+++ src/main/java/dev/petproject/twitter/user/tweet/mapper/impl/TweetAddRequestToTweetMapperImpl.java
@@ -1,16 +1,23 @@
 package dev.petproject.twitter.user.tweet.mapper.impl;
 
+import dev.petproject.twitter.user.profile.api.service.CurrentUserProfileApiService;
 import dev.petproject.twitter.user.tweet.mapper.TweetAddRequestToTweetMapper;
 import dev.petproject.twitter.user.tweet.model.Tweet;
 import dev.petproject.twitter.user.tweet.web.model.TweetAddRequest;
 
 public class TweetAddRequestToTweetMapperImpl implements TweetAddRequestToTweetMapper {
 
+    private final CurrentUserProfileApiService currentUserProfileApiService;
+
+    public TweetAddRequestToTweetMapperImpl(CurrentUserProfileApiService currentUserProfileApiService) {
+        this.currentUserProfileApiService = currentUserProfileApiService;
+    }
+
     @Override
     public Tweet map(TweetAddRequest tweetAddRequest) {
         Tweet tweet = new Tweet();
         tweet.setMessage(tweetAddRequest.message());
-        tweet.setUserProfile();
+        tweet.setUserProfile(this.currentUserProfileApiService.currentUserProfile());
 
         return tweet;
     }
