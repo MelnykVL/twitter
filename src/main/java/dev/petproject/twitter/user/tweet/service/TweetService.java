371d6ea89b656ac374e903d64ce86d61ca53a39e
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 00:15:18 +0200

    Created TweetService.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/service/TweetService.java
@@ -0,0 +1,8 @@
+package dev.petproject.twitter.user.tweet.service;
+
+import dev.petproject.twitter.user.tweet.model.Tweet;
+
+public interface TweetService {
+
+    Tweet createTweet(Tweet tweet);
+}
