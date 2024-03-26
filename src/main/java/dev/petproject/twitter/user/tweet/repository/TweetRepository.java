41a570816613c78732d5fa3772e4d4a2f51ee1d7
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 00:15:10 +0200

    Created TweetRepository.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/repository/TweetRepository.java
@@ -0,0 +1,6 @@
+package dev.petproject.twitter.user.tweet.repository;
+
+import dev.petproject.twitter.user.tweet.model.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TweetRepository extends JpaRepository<Tweet, Long> {}
