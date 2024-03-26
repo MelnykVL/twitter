ecfbcfe2fe8a9fd919446d268dea4fda56afb855
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 01:37:05 +0200

    Added min constraint for message of TweetAddRequest.java

--- src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetAddRequest.java
+++ src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetAddRequest.java
@@ -3,4 +3,4 @@ package dev.petproject.twitter.user.tweet.web.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record TweetAddRequest(@NotBlank @Size(max = 280) String message) {}
+public record TweetAddRequest(@NotBlank @Size(min = 1, max = 280) String message) {}
