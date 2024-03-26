d084a37975195aaa318c7bd9a06699fced49bb8f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 01:01:29 +0200

    Created CurrentUserProfileApiService.java

---
+++ src/main/java/dev/petproject/twitter/user/profile/api/service/CurrentUserProfileApiService.java
@@ -0,0 +1,8 @@
+package dev.petproject.twitter.user.profile.api.service;
+
+import dev.petproject.twitter.user.profile.model.UserProfile;
+
+public interface CurrentUserProfileApiService {
+
+    UserProfile currentUserProfile();
+}
