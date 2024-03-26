502d665d3fbb76b178289483e24b76572bc086fd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 00:15:55 +0200

    Created TweetAddUseCase.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/usecase/TweetAddUseCase.java
@@ -0,0 +1,9 @@
+package dev.petproject.twitter.user.tweet.usecase;
+
+import dev.petproject.twitter.user.tweet.web.model.TweetAddRequest;
+import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
+
+public interface TweetAddUseCase {
+
+    TweetResponse addTweet(TweetAddRequest tweetAddRequest);
+}
