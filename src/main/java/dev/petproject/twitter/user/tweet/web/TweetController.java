7508a301b8306c02cd51afd6357f91d56ec16321
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 23:08:00 +0200

    Added editTweet() method to TweetController.java

--- src/main/java/dev/petproject/twitter/user/tweet/web/TweetController.java
+++ src/main/java/dev/petproject/twitter/user/tweet/web/TweetController.java
@@ -1,7 +1,9 @@
 package dev.petproject.twitter.user.tweet.web;
 
 import dev.petproject.twitter.user.tweet.usecase.TweetAddUseCase;
+import dev.petproject.twitter.user.tweet.usecase.TweetEditUseCase;
 import dev.petproject.twitter.user.tweet.web.model.TweetAddRequest;
+import dev.petproject.twitter.user.tweet.web.model.TweetEditRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -12,14 +14,21 @@ import org.springframework.web.bind.annotation.*;
 public class TweetController {
 
     private final TweetAddUseCase tweetAddUseCase;
+    private final TweetEditUseCase tweetEditUseCase;
 
-    public TweetController(TweetAddUseCase tweetAddUseCase) {
+    public TweetController(TweetAddUseCase tweetAddUseCase, TweetEditUseCase tweetEditUseCase) {
         this.tweetAddUseCase = tweetAddUseCase;
+        this.tweetEditUseCase = tweetEditUseCase;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public TweetResponse addTweet(@Valid @RequestBody TweetAddRequest tweetAddRequest) {
         return this.tweetAddUseCase.addTweet(tweetAddRequest);
+    }
+
+    @PutMapping
+    public TweetResponse editTweet(@Valid @RequestBody TweetEditRequest tweetEditRequest) {
+        return this.tweetEditUseCase.editTweet(tweetEditRequest);
     }
 }
