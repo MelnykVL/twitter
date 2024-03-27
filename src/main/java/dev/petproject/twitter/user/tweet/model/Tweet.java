e08e95a94af501c914e286efc6bcd3aa5ce3e446
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 22:16:10 +0200

    Added modifiedTimestamp field to the Tweet model

--- src/main/java/dev/petproject/twitter/user/tweet/model/Tweet.java
+++ src/main/java/dev/petproject/twitter/user/tweet/model/Tweet.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
@@ -23,6 +24,9 @@ public class Tweet {
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private Instant createdTimestamp;
+    @LastModifiedDate
+    @Column
+    private Instant modifiedTimestamp;
     @ManyToOne(optional = false)
     private UserProfile userProfile;
 }
