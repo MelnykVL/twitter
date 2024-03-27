c4e04f19fa55d97ffa6c9ee46cbfd8a321137c15
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 22:15:05 +0200

    Created TweetEditRequest.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetEditRequest.java
@@ -0,0 +1,7 @@
+package dev.petproject.twitter.user.tweet.web.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record TweetEditRequest(@NotNull long id, @NotBlank @Size(min = 1, max = 280) String message) {}
