f943d458c472546fe87f470e39312b22016eed36
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 23:01:13 +0200

    Added modifiedTimestamp field to TweetResponse.java

--- src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetResponse.java
+++ src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetResponse.java
@@ -2,4 +2,4 @@ package dev.petproject.twitter.user.tweet.web.model;
 
 import java.time.Instant;
 
-public record TweetResponse(long id, String message, Instant createdTimestamp) {}
+public record TweetResponse(long id, String message, Instant createdTimestamp, Instant modifiedTimestamp) {}
