6844d895caeda7f91816540e018b61550128e079
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 22:35:56 +0200

    Created TweetEditRequestToTweetMapperImpl.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/mapper/impl/TweetEditRequestToTweetMapperImpl.java
@@ -0,0 +1,29 @@
+package dev.petproject.twitter.user.tweet.mapper.impl;
+
+import dev.petproject.twitter.user.tweet.mapper.TweetEditRequestToTweetMapper;
+import dev.petproject.twitter.user.tweet.model.Tweet;
+import dev.petproject.twitter.user.tweet.service.TweetService;
+import dev.petproject.twitter.user.tweet.web.model.TweetEditRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TweetEditRequestToTweetMapperImpl implements TweetEditRequestToTweetMapper {
+
+    private final TweetService tweetService;
+
+    public TweetEditRequestToTweetMapperImpl(TweetService tweetService) {
+        this.tweetService = tweetService;
+    }
+
+    @Override
+    public Tweet map(TweetEditRequest tweetEditRequest) {
+        Tweet currentTweet = this.tweetService.findTweetById(tweetEditRequest.id())
+                .orElseThrow(() -> {
+                    String errorMessage = String.format("Tweet with Id = %d does not exist.", tweetEditRequest.id());
+                    return new RuntimeException(errorMessage);
+                });
+        currentTweet.setMessage(tweetEditRequest.message());
+
+        return null;
+    }
+}
