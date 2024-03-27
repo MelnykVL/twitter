8bc1dc20c912d59b82dd0df4c40e3d5f368bcd49
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-03-27 22:15:36 +0200

    Created TweetEditRequestToTweetMapper.java

---
+++ src/main/java/dev/petproject/twitter/user/tweet/mapper/TweetEditRequestToTweetMapper.java
@@ -0,0 +1,7 @@
+package dev.petproject.twitter.user.tweet.mapper;
+
+import dev.petproject.twitter.security.mapper.Mapper;
+import dev.petproject.twitter.user.tweet.model.Tweet;
+import dev.petproject.twitter.user.tweet.web.model.TweetEditRequest;
+
+public interface TweetEditRequestToTweetMapper extends Mapper<Tweet, TweetEditRequest> {}
