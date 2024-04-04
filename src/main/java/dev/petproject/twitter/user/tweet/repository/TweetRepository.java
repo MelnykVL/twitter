bd0409779e0d070f92acc2d45aef8c7a537484e0
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-04 22:39:03 +0300

    Added findAllByUserProfile() method to TweetRepository interface

--- src/main/java/dev/petproject/twitter/user/tweet/repository/TweetRepository.java
+++ src/main/java/dev/petproject/twitter/user/tweet/repository/TweetRepository.java
@@ -1,7 +1,12 @@
 package dev.petproject.twitter.user.tweet.repository;
 
+import dev.petproject.twitter.user.profile.model.UserProfile;
 import dev.petproject.twitter.user.tweet.model.Tweet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+    List<Tweet> findAllByUserProfile(UserProfile owner);
 }
