0c6bd66f099b388983795a9375a05e9e1b70cdb3
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-04 22:53:51 +0300

    Added findOwnerTweets() method to TweetController.java

--- src/main/java/dev/petproject/twitter/user/tweet/web/TweetController.java
+++ src/main/java/dev/petproject/twitter/user/tweet/web/TweetController.java
@@ -3,12 +3,14 @@ package dev.petproject.twitter.user.tweet.web;
 import dev.petproject.twitter.user.tweet.usecase.TweetAddUseCase;
 import dev.petproject.twitter.user.tweet.usecase.TweetDeleteUseCase;
 import dev.petproject.twitter.user.tweet.usecase.TweetEditUseCase;
+import dev.petproject.twitter.user.tweet.usecase.TweetFindUseCase;
 import dev.petproject.twitter.user.tweet.web.model.TweetAddRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetEditRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/tweets")
@@ -24,12 +27,14 @@ public class TweetController {
   private final TweetAddUseCase tweetAddUseCase;
   private final TweetEditUseCase tweetEditUseCase;
   private final TweetDeleteUseCase tweetDeleteUseCase;
+  private final TweetFindUseCase tweetFindUseCase;
 
   public TweetController(TweetAddUseCase tweetAddUseCase, TweetEditUseCase tweetEditUseCase,
-      TweetDeleteUseCase tweetDeleteUseCase) {
+      TweetDeleteUseCase tweetDeleteUseCase, TweetFindUseCase tweetFindUseCase) {
     this.tweetAddUseCase = tweetAddUseCase;
     this.tweetEditUseCase = tweetEditUseCase;
     this.tweetDeleteUseCase = tweetDeleteUseCase;
+    this.tweetFindUseCase = tweetFindUseCase;
   }
 
   @PostMapping
@@ -44,8 +49,12 @@ public class TweetController {
   }
 
   @DeleteMapping("/{tweetId}")
-  @ResponseStatus(HttpStatus.ACCEPTED)
   public void deleteTweet(@PathVariable long tweetId) {
     tweetDeleteUseCase.deleteTweet(tweetId);
+  }
+
+  @GetMapping
+  public List<TweetResponse> findOwnerTweets() {
+    return this.tweetFindUseCase.findTweets();
   }
 }
