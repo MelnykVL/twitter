ebf02d2288985005e951ecd8e1d91b9b63c65773
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-07 23:41:41 +0300

    Added Pageable and Sort into findTweets() method of TweetFindUseCaseFacade.java

--- src/main/java/dev/petproject/twitter/user/tweet/usecase/impl/TweetFindUseCaseFacade.java
+++ src/main/java/dev/petproject/twitter/user/tweet/usecase/impl/TweetFindUseCaseFacade.java
@@ -7,8 +7,13 @@ import dev.petproject.twitter.user.tweet.service.TweetService;
 import dev.petproject.twitter.user.tweet.usecase.TweetFindUseCase;
 import dev.petproject.twitter.user.tweet.web.model.TweetFindRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import java.util.List;
+
+import static dev.petproject.twitter.user.tweet.model.Tweet_.CREATED_TIMESTAMP;
 
 @Component
 public class TweetFindUseCaseFacade implements TweetFindUseCase {
@@ -27,7 +32,9 @@ public class TweetFindUseCaseFacade implements TweetFindUseCase {
   @Override
   public List<TweetResponse> findTweets(TweetFindRequest tweetFindRequest) {
     UserProfile owner = this.currentUserProfileApiService.currentUserProfile();
-    return this.tweetService.findAllTweets(owner)
+    Sort sort = Sort.by(Sort.Direction.DESC, CREATED_TIMESTAMP);
+    Pageable pageable = PageRequest.of(tweetFindRequest.page(), tweetFindRequest.limit(), sort);
+    return this.tweetService.findAllTweets(owner, pageable)
         .stream()
         .map(tweetToTweetResponseMapper::map)
         .toList();
