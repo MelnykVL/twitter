04d15862243f1cda356652e8a69966a178e50bd4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-07 23:08:08 +0300

    Added TweetFindRequest as parameter to findTweets() method of TweetFindUseCaseFacade.java

--- src/main/java/dev/petproject/twitter/user/tweet/usecase/impl/TweetFindUseCaseFacade.java
+++ src/main/java/dev/petproject/twitter/user/tweet/usecase/impl/TweetFindUseCaseFacade.java
@@ -5,6 +5,7 @@ import dev.petproject.twitter.user.profile.model.UserProfile;
 import dev.petproject.twitter.user.tweet.mapper.TweetToTweetResponseMapper;
 import dev.petproject.twitter.user.tweet.service.TweetService;
 import dev.petproject.twitter.user.tweet.usecase.TweetFindUseCase;
+import dev.petproject.twitter.user.tweet.web.model.TweetFindRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
 import org.springframework.stereotype.Component;
 import java.util.List;
@@ -24,7 +25,7 @@ public class TweetFindUseCaseFacade implements TweetFindUseCase {
   }
 
   @Override
-  public List<TweetResponse> findTweets() {
+  public List<TweetResponse> findTweets(TweetFindRequest tweetFindRequest) {
     UserProfile owner = this.currentUserProfileApiService.currentUserProfile();
     return this.tweetService.findAllTweets(owner)
         .stream()
