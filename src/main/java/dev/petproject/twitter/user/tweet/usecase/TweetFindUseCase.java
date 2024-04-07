dc5177662bde4263d2d07a55c99deb6a029de06f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-07 23:17:03 +0300

    Added Validated and Valid annotations to TweetFindUseCase.java

--- src/main/java/dev/petproject/twitter/user/tweet/usecase/TweetFindUseCase.java
+++ src/main/java/dev/petproject/twitter/user/tweet/usecase/TweetFindUseCase.java
@@ -2,9 +2,12 @@ package dev.petproject.twitter.user.tweet.usecase;
 
 import dev.petproject.twitter.user.tweet.web.model.TweetFindRequest;
 import dev.petproject.twitter.user.tweet.web.model.TweetResponse;
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import java.util.List;
 
+@Validated
 public interface TweetFindUseCase {
 
-  List<TweetResponse> findTweets(TweetFindRequest tweetFindRequest);
+  List<TweetResponse> findTweets(@Valid TweetFindRequest tweetFindRequest);
 }
