67308c0d79fa6438ba5752d7ca63f00e3c208958
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-08 23:33:39 +0300

    Added totalTweets, isFirstPage, isLastPage fields to TweetPageResponse record

--- src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetPageResponse.java
+++ src/main/java/dev/petproject/twitter/user/tweet/web/model/TweetPageResponse.java
@@ -2,4 +2,5 @@ package dev.petproject.twitter.user.tweet.web.model;
 
 import java.util.List;
 
-public record TweetPageResponse(List<TweetResponse> tweets) { }
+public record TweetPageResponse(long totalTweets, boolean isFirstPage, boolean isLastPage,
+                                List<TweetResponse> tweets) { }
