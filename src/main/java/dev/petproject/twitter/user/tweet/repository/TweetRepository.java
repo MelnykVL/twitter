b32531cbc7ac3b569dfa6b2b1535bc4eca2da8b5
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-08 23:29:27 +0300

    Changed a returning type of findAllByUserProfile() method of TweetRepository interface from List to Page

--- src/main/java/dev/petproject/twitter/user/tweet/repository/TweetRepository.java
+++ src/main/java/dev/petproject/twitter/user/tweet/repository/TweetRepository.java
@@ -2,11 +2,11 @@ package dev.petproject.twitter.user.tweet.repository;
 
 import dev.petproject.twitter.user.profile.model.UserProfile;
 import dev.petproject.twitter.user.tweet.model.Tweet;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
 
-  List<Tweet> findAllByUserProfile(UserProfile owner, Pageable pageable);
+  Page<Tweet> findAllByUserProfile(UserProfile owner, Pageable pageable);
 }
