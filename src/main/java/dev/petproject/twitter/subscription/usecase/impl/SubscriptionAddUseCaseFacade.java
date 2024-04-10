df8708b3d397181665a31fd04e86ed1cf21d4259
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 00:26:04 +0300

    Created SubscriptionAddUseCaseFacade

---
+++ src/main/java/dev/petproject/twitter/subscription/usecase/impl/SubscriptionAddUseCaseFacade.java
@@ -0,0 +1,38 @@
+package dev.petproject.twitter.subscription.usecase.impl;
+
+import dev.petproject.twitter.subscription.mapper.SubscribeRequestToSubscriptionMapper;
+import dev.petproject.twitter.subscription.model.Subscription;
+import dev.petproject.twitter.subscription.service.SubscriptionService;
+import dev.petproject.twitter.subscription.usecase.SubscriptionAddUseCase;
+import dev.petproject.twitter.subscription.web.model.SubscribeRequest;
+import dev.petproject.twitter.user.profile.model.UserProfile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscriptionAddUseCaseFacade implements SubscriptionAddUseCase {
+
+  private final SubscribeRequestToSubscriptionMapper subscriptionMapper;
+  private final SubscriptionService subscriptionService;
+
+  public SubscriptionAddUseCaseFacade(SubscribeRequestToSubscriptionMapper subscriptionMapper,
+      SubscriptionService subscriptionService) {
+    this.subscriptionMapper = subscriptionMapper;
+    this.subscriptionService = subscriptionService;
+  }
+
+  @Override
+  public void subscribe(SubscribeRequest subscribeRequest) {
+    Subscription subscription = this.subscriptionMapper.map(subscribeRequest);
+    UserProfile follower = subscription.getFollower();
+    UserProfile followed = subscription.getFollowed();
+
+    if (follower.equals(followed)) {
+      throw new RuntimeException("You cannot subscribe to yourself!");
+    }
+    if (this.subscriptionService.existsSubscription(subscription)) {
+      String errorMessage = String.format("You're already subscribed to %s.", followed.getNickname());
+      throw new RuntimeException(errorMessage);
+    }
+    this.subscriptionService.createSubscription(subscription);
+  }
+}
