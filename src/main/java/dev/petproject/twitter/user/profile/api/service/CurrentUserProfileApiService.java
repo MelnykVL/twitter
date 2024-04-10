ed820d4edd55b2efaf1f40ac67daf2a962d9beb2
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 00:38:48 +0300

    Removed CurrentUserProfileApiService interface

--- src/main/java/dev/petproject/twitter/user/profile/api/service/CurrentUserProfileApiService.java
+++
@@ -1,8 +0,0 @@
-package dev.petproject.twitter.user.profile.api.service;
-
-import dev.petproject.twitter.user.profile.model.UserProfile;
-
-public interface CurrentUserProfileApiService {
-
-  UserProfile currentUserProfile();
-}
