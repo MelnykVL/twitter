7bbd5410785ef52fdba6106963ebc46924996ca9
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 00:31:26 +0300

    Changed variable type of followedId from long to Long

--- src/main/java/dev/petproject/twitter/subscription/web/model/SubscribeRequest.java
+++ src/main/java/dev/petproject/twitter/subscription/web/model/SubscribeRequest.java
@@ -2,4 +2,4 @@ package dev.petproject.twitter.subscription.web.model;
 
 import jakarta.validation.constraints.NotNull;
 
-public record SubscribeRequest(@NotNull long followedId) { }
+public record SubscribeRequest(@NotNull Long followedId) { }
