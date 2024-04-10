41d1a4293d0d1bb970641183ca2955baa80ce235
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 01:32:56 +0300

    Added findByFollowerAndFollowed() method to SubscriptionRepository interface

--- src/main/java/dev/petproject/twitter/subscription/repository/SubscriptionRepository.java
+++ src/main/java/dev/petproject/twitter/subscription/repository/SubscriptionRepository.java
@@ -3,8 +3,11 @@ package dev.petproject.twitter.subscription.repository;
 import dev.petproject.twitter.subscription.model.Subscription;
 import dev.petproject.twitter.user.profile.model.UserProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
   boolean existsByFollowerAndFollowed(UserProfile follower, UserProfile followed);
+
+  Optional<Subscription> findByFollowerAndFollowed(UserProfile follower, UserProfile followedo);
 }
