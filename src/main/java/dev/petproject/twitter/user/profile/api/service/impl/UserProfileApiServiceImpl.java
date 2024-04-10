249faffa330c4704bc3a2bac9d727091608a95d5
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 00:53:50 +0300

    Renamed CurrentUserProfileApiServiceImpl to UserProfileApiServiceImpl and implemented findUserProfileById() method

--- src/main/java/dev/petproject/twitter/user/profile/api/service/impl/UserProfileApiServiceImpl.java
+++ src/main/java/dev/petproject/twitter/user/profile/api/service/impl/UserProfileApiServiceImpl.java
@@ -2,19 +2,18 @@ package dev.petproject.twitter.user.profile.api.service.impl;
 
 import dev.petproject.twitter.security.api.model.CurrentUserApiModel;
 import dev.petproject.twitter.security.api.service.IdentityApiService;
-import dev.petproject.twitter.user.profile.api.service.CurrentUserProfileApiService;
+import dev.petproject.twitter.user.profile.api.service.UserProfileApiService;
 import dev.petproject.twitter.user.profile.model.UserProfile;
 import dev.petproject.twitter.user.profile.service.UserProfileService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CurrentUserProfileApiServiceImpl implements CurrentUserProfileApiService {
+public class UserProfileApiServiceImpl implements UserProfileApiService {
 
   private final IdentityApiService identityApiService;
   private final UserProfileService userProfileService;
 
-  public CurrentUserProfileApiServiceImpl(IdentityApiService identityApiService,
-      UserProfileService userProfileService) {
+  public UserProfileApiServiceImpl(IdentityApiService identityApiService, UserProfileService userProfileService) {
     this.identityApiService = identityApiService;
     this.userProfileService = userProfileService;
   }
@@ -23,11 +22,11 @@ public class CurrentUserProfileApiServiceImpl implements CurrentUserProfileApiSe
   public UserProfile currentUserProfile() {
     CurrentUserApiModel currentUser = this.identityApiService.currentUserAccount()
         .orElseThrow(() -> new RuntimeException("The user must be authorized in the system"));
+    return this.userProfileService.findUserProfileById(currentUser.userAccountId());
+  }
 
-    return this.userProfileService.findUserProfileById(currentUser.userAccountId())
-        .orElseThrow(() -> {
-          String errorMessage = String.format("User with Id = %d does not exist", currentUser.userAccountId());
-          throw new RuntimeException(errorMessage);
-        });
+  @Override
+  public UserProfile findUserProfileById(long userProfileId) {
+    return this.userProfileService.findUserProfileById(userProfileId);
   }
 }
