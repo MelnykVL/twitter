f61499d2bdfada3ccc02d109d3c1266c9bea3ef4
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-10 23:57:22 +0300

    Formatted code in whole project

--- src/main/java/dev/petproject/twitter/subscription/usecase/SubscriptionAddUseCase.java
+++ src/main/java/dev/petproject/twitter/subscription/usecase/SubscriptionAddUseCase.java
@@ -3,5 +3,6 @@ package dev.petproject.twitter.subscription.usecase;
 import dev.petproject.twitter.subscription.web.model.SubscribeRequest;
 
 public interface SubscriptionAddUseCase {
+
   void subscribe(SubscribeRequest subscribeRequest);
 }
