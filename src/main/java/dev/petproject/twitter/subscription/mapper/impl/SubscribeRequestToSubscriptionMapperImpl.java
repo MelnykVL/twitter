f9dfc0c3febcf0241049ef422cdb4d88d7f84a85
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-11 00:52:08 +0300

    Created SubscribeRequestToSubscriptionMapperImpl

---
+++ src/main/java/dev/petproject/twitter/subscription/mapper/impl/SubscribeRequestToSubscriptionMapperImpl.java
@@ -0,0 +1,29 @@
+package dev.petproject.twitter.subscription.mapper.impl;
+
+import dev.petproject.twitter.subscription.mapper.SubscribeRequestToSubscriptionMapper;
+import dev.petproject.twitter.subscription.model.Subscription;
+import dev.petproject.twitter.subscription.web.model.SubscribeRequest;
+import dev.petproject.twitter.user.profile.api.service.UserProfileApiService;
+import dev.petproject.twitter.user.profile.model.UserProfile;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscribeRequestToSubscriptionMapperImpl implements SubscribeRequestToSubscriptionMapper {
+
+  private final UserProfileApiService userProfileApiService;
+
+  public SubscribeRequestToSubscriptionMapperImpl(UserProfileApiService userProfileApiService) {
+    this.userProfileApiService = userProfileApiService;
+  }
+
+  @Override
+  public Subscription map(SubscribeRequest subscribeRequest) {
+    UserProfile follower = this.userProfileApiService.currentUserProfile();
+    UserProfile followed = this.userProfileApiService.findUserProfileById(subscribeRequest.followedId());
+    Subscription subscription = new Subscription();
+    subscription.setFollower(follower);
+    subscription.setFollowed(followed);
+
+    return subscription;
+  }
+}
