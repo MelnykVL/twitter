09fcdceb6b0b4efa0beed7f3d45c63be2ad423af
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-10 23:52:59 +0300

    Created SubscriptionAddUseCase interface

---
+++ src/main/java/dev/petproject/twitter/subscription/usecase/SubscriptionAddUseCase.java
@@ -0,0 +1,7 @@
+package dev.petproject.twitter.subscription.usecase;
+
+import dev.petproject.twitter.subscription.web.model.SubscribeRequest;
+
+public interface SubscriptionAddUseCase {
+  void subscribe(SubscribeRequest subscribeRequest);
+}
