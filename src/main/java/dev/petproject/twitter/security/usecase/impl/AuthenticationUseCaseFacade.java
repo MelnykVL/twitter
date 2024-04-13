661f064e547f1974131eeb111a51ca890be83c44
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-13 23:53:51 +0300

    Created AuthenticationUseCaseFacade.java

---
+++ src/main/java/dev/petproject/twitter/security/usecase/impl/AuthenticationUseCaseFacade.java
@@ -0,0 +1,33 @@
+package dev.petproject.twitter.security.usecase.impl;
+
+import dev.petproject.twitter.security.service.AccessTokenService;
+import dev.petproject.twitter.security.usecase.AuthenticationUseCase;
+import dev.petproject.twitter.security.web.model.AccessToken;
+import dev.petproject.twitter.security.web.model.LoginRequest;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationUseCaseFacade implements AuthenticationUseCase {
+
+  private final AuthenticationManager authenticationManager;
+  private final AccessTokenService accessTokenService;
+
+  public AuthenticationUseCaseFacade(AuthenticationManager authenticationManager,
+      AccessTokenService accessTokenService) {
+    this.authenticationManager = authenticationManager;
+    this.accessTokenService = accessTokenService;
+  }
+
+  @Override
+  public AccessToken authenticate(LoginRequest loginRequest) {
+    Authentication authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.username(),
+        loginRequest.password());
+    Authentication authentication = this.authenticationManager.authenticate(authenticationToken);
+    String idToken = this.accessTokenService.generateIdToken(authentication);
+
+    return new AccessToken(idToken);
+  }
+}
