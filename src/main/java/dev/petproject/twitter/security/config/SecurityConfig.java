c56b21689c93855c11691486bd9dd7350420c4ee
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-13 23:43:23 +0300

    Set up authenticationManager, JwtDecoder,JwtEncoder and JWKSource beans to SecurityConfig.java

--- src/main/java/dev/petproject/twitter/security/config/SecurityConfig.java
+++ src/main/java/dev/petproject/twitter/security/config/SecurityConfig.java
@@ -1,25 +1,46 @@
 package dev.petproject.twitter.security.config;
 
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  private final RsaKeys rsaKey;
+
+  public SecurityConfig(RsaKeys rsaKey) { this.rsaKey = rsaKey; }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     httpSecurity.csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/accounts/register")
+            .permitAll()
+            .requestMatchers("/api/v1/authentication/access_token")
             .permitAll()
             .requestMatchers("/error")
             .permitAll()
@@ -29,13 +50,43 @@ public class SecurityConfig {
             .hasRole("ADMIN")
             .anyRequest()
             .authenticated())
-        .httpBasic(Customizer.withDefaults());
+        .httpBasic(Customizer.withDefaults())
+        .oauth2ResourceServer(oauth -> oauth.jwt(jwt -> jwt.decoder(jwtDecoder())));
 
     return httpSecurity.build();
   }
 
   @Bean
+  @SneakyThrows
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) {
+    return configuration.getAuthenticationManager();
+  }
+
+  @Bean
   public PasswordEncoder passwordEncoder() {
     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+
+  @Bean
+  public JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withPublicKey(this.rsaKey.rsaPublicKey())
+        .build();
+  }
+
+  @Bean
+  public JwtEncoder jwtEncoder() {
+    return new NimbusJwtEncoder(jwkSource());
+  }
+
+  @Bean
+  public JWKSource<SecurityContext> jwkSource() {
+    RSAPublicKey publicKey = this.rsaKey.rsaPublicKey();
+    RSAPrivateKey privateKey = this.rsaKey.rsaPrivateKey();
+    RSAKey rsaKey = new RSAKey.Builder(publicKey).privateKey(privateKey)
+        .keyID(UUID.randomUUID()
+            .toString())
+        .build();
+    JWKSet jwkSet = new JWKSet(rsaKey);
+    return new ImmutableJWKSet<>(jwkSet);
   }
 }
