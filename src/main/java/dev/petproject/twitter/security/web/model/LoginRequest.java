7a489e639ccd53182d7ec00c7ab2497992e78278
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-13 22:54:19 +0300

    Created LoginRequest record

---
+++ src/main/java/dev/petproject/twitter/security/web/model/LoginRequest.java
@@ -0,0 +1,6 @@
+package dev.petproject.twitter.security.web.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(@Email @NotBlank String username, @NotBlank String password) { }
