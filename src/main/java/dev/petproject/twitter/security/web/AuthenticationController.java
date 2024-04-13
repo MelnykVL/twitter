e4c355c7d697a2fcbae227cffb33f4571552d4f0
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-13 23:21:05 +0300

    Created AuthenticationController

---
+++ src/main/java/dev/petproject/twitter/security/web/AuthenticationController.java
@@ -0,0 +1,26 @@
+package dev.petproject.twitter.security.web;
+
+import dev.petproject.twitter.security.usecase.AuthenticationUseCase;
+import dev.petproject.twitter.security.web.model.AccessToken;
+import dev.petproject.twitter.security.web.model.LoginRequest;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/authentication")
+public class AuthenticationController {
+
+  private final AuthenticationUseCase authenticationUseCase;
+
+  public AuthenticationController(AuthenticationUseCase authenticationUseCase) {
+    this.authenticationUseCase = authenticationUseCase;
+  }
+
+  @PostMapping("/access_token")
+  public AccessToken getToken(@Valid @RequestBody LoginRequest loginRequest) {
+    return this.authenticationUseCase.authenticate(loginRequest);
+  }
+}
