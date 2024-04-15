e020cb4ab0f87517e78a52594d55529ec69bcccd
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-15 23:19:59 +0300

    Added assertions to TwitterApplicationTests

--- src/test/java/dev/petproject/twitter/TwitterApplicationTests.java
+++ src/test/java/dev/petproject/twitter/TwitterApplicationTests.java
@@ -1,11 +1,20 @@
 package dev.petproject.twitter;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class TwitterApplicationTests {
 
+  @Autowired
+  private ApplicationContext context;
+
   @Test
-  void contextLoads() { }
+  void contextLoads() {
+    assertNotNull(context);
+  }
 }
