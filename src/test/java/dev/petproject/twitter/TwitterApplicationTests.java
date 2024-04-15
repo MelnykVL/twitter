f6911b37b8c263b5d5378c4a672aaee17f608517
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-15 23:02:45 +0300

    Created default test for TwitterApplicationTests

---
+++ src/test/java/dev/petproject/twitter/TwitterApplicationTests.java
@@ -0,0 +1,11 @@
+package dev.petproject.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterApplicationTests {
+
+  @Test
+  void contextLoads() { }
+}
