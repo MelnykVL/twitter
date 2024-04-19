15c1f685951724532e3725b6323a79fb157c3703
MelnykVL <w.vladyslavmelnyk@gmail.com>
2024-04-19 20:27:51 +0300

    Changed returning type from Optional<UserRole> to UserRole in UserRoleService interface

--- src/main/java/dev/petproject/twitter/security/service/UserRoleService.java
+++ src/main/java/dev/petproject/twitter/security/service/UserRoleService.java
@@ -1,9 +1,8 @@
 package dev.petproject.twitter.security.service;
 
 import dev.petproject.twitter.security.model.UserRole;
-import java.util.Optional;
 
 public interface UserRoleService {
 
-  Optional<UserRole> findUserRole();
+  UserRole findUserRole();
 }
