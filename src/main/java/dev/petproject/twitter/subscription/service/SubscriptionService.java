a056078abfa9a318966370127cd4a499982309d0
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-02-24 22:59:29 +0100

    Added findAllFollowerSubscriptions() method in SubscriptionService.java

--- src/main/java/dev/petproject/twitter/subscription/service/SubscriptionService.java
+++ src/main/java/dev/petproject/twitter/subscription/service/SubscriptionService.java
@@ -1,6 +1,10 @@
 package dev.petproject.twitter.subscription.service;
 
+import dev.petproject.twitter.subscription.model.FollowerSubscription;
 import dev.petproject.twitter.subscription.model.Subscription;
+import dev.petproject.twitter.user.profile.model.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface SubscriptionService {
 
@@ -9,4 +13,6 @@ public interface SubscriptionService {
   void deleteSubscription(Subscription subscription);
 
   boolean existsSubscription(Subscription subscription);
+
+  Page<FollowerSubscription> findAllFollowerSubscriptions(UserProfile author, Pageable pageable);
 }
