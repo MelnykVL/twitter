d4c52259a3f4699d21bc511cf6827d0d74cf288f
MelnykVL <w.vladyslavmelnyk@gmail.com>
2025-02-24 22:59:16 +0100

    Added findAllByFollowed() method in SubscriptionRepository.java

--- src/main/java/dev/petproject/twitter/subscription/repository/SubscriptionRepository.java
+++ src/main/java/dev/petproject/twitter/subscription/repository/SubscriptionRepository.java
@@ -1,7 +1,10 @@
 package dev.petproject.twitter.subscription.repository;
 
+import dev.petproject.twitter.subscription.model.FollowerSubscription;
 import dev.petproject.twitter.subscription.model.Subscription;
 import dev.petproject.twitter.user.profile.model.UserProfile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
@@ -10,4 +13,6 @@ public interface SubscriptionRepository extends JpaRepository<Subscription, Long
   boolean existsByFollowerAndFollowed(UserProfile follower, UserProfile followed);
 
   Optional<Subscription> findByFollowerAndFollowed(UserProfile follower, UserProfile followedo);
+
+  Page<FollowerSubscription> findAllByFollowed(UserProfile author, Pageable pageable);
 }
